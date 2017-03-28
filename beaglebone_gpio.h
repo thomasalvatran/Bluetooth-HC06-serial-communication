f23040332ef84c4a20638c9d5bcc66a05dbe2e92
thomasalvatran <thomasalvatran@gmail.com>
2017-03-28 00:26:03 -0400

    Include file Mar 2017

---
+++ beaglebone_gpio.h
@@ -0,0 +1,18 @@
+#ifndef _BEAGLEBONE_GPIO_H_
+#define _BEAGLEBONE_GPIO_H_
+
+#define GPIO1_START_ADDR 0x4804C000
+#define GPIO1_END_ADDR 0x4804DFFF
+#define GPIO1_SIZE (GPIO1_END_ADDR - GPIO1_START_ADDR)
+#define GPIO_OE 0x134
+#define GPIO_DATAOUT 0x13C
+#define GPIO_SETDATAOUT 0x194
+#define GPIO_CLEARDATAOUT 0x190
+
+#define USR0_LED (1<<21)
+#define USR1_LED (1<<22)
+#define USR2_LED (1<<23)
+#define USR3_LED (1<<24) //GPIO1_24
+#define LED12  (1<<28) //GPIO1_28
+#define LED15  (1<<16)
+#endif
